a378949b6f0eb529bf7bdce8a7aa2871ce426afe
Michael Greeff <unmodster@gmail.com>
2019-07-16 19:15:07 -0600

    67 Errors
    I guess this tute is a little out of date. When will I learn to check the publish date.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D> ();        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+        rb2D.AddForce(movement);
+        
+    }
+}
